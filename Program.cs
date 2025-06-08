1e8078c8abce88deb203cda18ca047cd8d1136e2
edpau <edpau@me.com>
2025-06-08 13:12:25 +0100

    feat: refactor checkerboard logic into Board class with dynamic size and print method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            
+            Board board = new Board(4, 4);
+            board.Print();
+        }
+    }
+
+    class Board
+    {
+        private int[,] checkers;
+        private int rows;
+        private int columns;
+
+        public Board(int r, int c)
+        {
+            rows = r;
+            columns = c;
+            checkers = new int[rows, columns];
+            Setup();
+        }
+
+        private void Setup()
+        {
+            bool squareValue = true;
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    checkers[r, c] = Convert.ToInt32(squareValue);
+                    squareValue = !squareValue;
+                }
+                squareValue = !squareValue;
+            }
+        }
+
+        public void Print()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    Console.Write($"{checkers[r, c]}");
+                }
+                Console.WriteLine();
             }
         }
     }
+}
